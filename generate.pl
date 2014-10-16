f8511ecf783017c1e5ff5929f7c6f27cdfeb11a4
Ryan Schmitt <rschmitt@pobox.com>
2014-10-15 21:10:29 -0700

    Convert code generator to Perl

---
+++ generate.pl
@@ -0,0 +1,167 @@
+#!/usr/bin/env perl -w
+
+use v5.16;
+use strict;
+
+sub main {
+  write_file("CrystalMethod.java", gen_builder_class());
+  for my $i (2..9) {
+    my $suffix = suffix($i);
+    write_file("Function$suffix.java", gen_functional_interface($i));
+    write_file("Multimethod$suffix.java", gen_multimethod_interface($i));
+  }
+}
+
+sub write_file {
+  my ($filename, $content) = @_;
+  open(my $fh, ">", "src/main/java/com/github/rschmitt/crystalmethod/gen/$filename")
+    or die "Unable to open $filename for write: $!";
+  print $fh $content;
+  close $fh;
+}
+
+sub gen_builder_class {
+  my @methods;
+  for my $i (1..9) {
+    push @methods, gen_builder_method($i);
+  }
+  my $m = join "\n", @methods;
+  return <<HERE
+package com.github.rschmitt.crystalmethod;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+public class CrystalMethod {
+$m
+}
+HERE
+}
+
+sub gen_builder_method {
+  my $count = shift;
+  my $multimethod = gen_multimethod($count);
+  my $dispatchFn = gen_function($count, "D");
+  my $function = gen_function($count, "R");
+  my $typeVars = gen_type_vars($count);
+  my $args = get_args($count);
+  return <<HERE
+    \@SuppressWarnings("unchecked")
+    public static <T extends $multimethod, D, R, $typeVars> T buildMultimethod(
+            $dispatchFn dispatchFn,
+            Map<D, $function> methods,
+            Class<T> type
+    ) {
+        Map<D, $function> myCopy = new HashMap<>();
+        myCopy.putAll(methods);
+        return (T) Proxy.newProxyInstance(Thread.currentThread().getContextClassLoader(),
+                new Class<?>[]{type},
+                (Object proxy, Method method, Object[] args) -> {
+                    if ("getDispatchFn".equals(method.getName()))
+                        return dispatchFn;
+                    else if ("getDispatchMap".equals(method.getName()))
+                        return Collections.unmodifiableMap(myCopy);
+                    D dispatchVal = dispatchFn.apply($args);
+                    return myCopy.get(dispatchVal).apply($args);
+                });
+    }
+HERE
+}
+
+sub get_args {
+  my $count = shift;
+  my @args;
+  for my $i (1..$count) {
+    my $idx = $i - 1;
+    push @args, "(T$i) args[$idx]";
+  }
+  return join ", ", @args;
+}
+
+# TODO MultimethodN should extend FunctionN
+sub gen_multimethod_interface {
+  my $count = shift;
+  my $args = gen_param_list($count);
+  my $function = gen_function($count, "R");
+  my $dispatchFn = gen_function($count, "D");
+  my $multimethod = gen_multimethod($count);
+  return <<HERE
+package com.github.rschmitt.crystalmethod;
+
+import java.util.Map;
+import java.util.function.Function;
+
+public interface $multimethod {
+    R invoke($args);
+
+    default Map<D, $function> getDispatchMap() {
+        return null;
+    }
+
+    default $dispatchFn getDispatchFn() {
+        return null;
+    }
+}
+HERE
+}
+
+sub gen_functional_interface {
+  my $count = shift;
+  my $function = gen_function($count, "R");
+  my $args = gen_param_list($count);
+return <<HERE
+package com.github.rschmitt.crystalmethod;
+
+\@FunctionalInterface
+public interface $function {
+    R apply($args);
+}
+HERE
+}
+
+sub gen_function {
+  my ($count, $returnType) = @_;
+  my $suffix = suffix($count);
+  my $typeVars = gen_type_vars($count);
+  return "Function$suffix<$typeVars, $returnType>";
+}
+
+sub gen_multimethod {
+  my ($count, $returnType) = @_;
+  my $suffix = suffix($count);
+  my $typeVars = gen_type_vars($count);
+  return "Multimethod$suffix<D, R, $typeVars>";
+}
+
+sub gen_type_vars {
+  my ($count) = @_;
+  my @vars;
+  for my $i (1..$count) {
+    push @vars, "T$i";
+  }
+  return join ", ", @vars;
+}
+
+sub gen_param_list {
+  my $count = shift;
+  my @params;
+  for my $i (1..$count) {
+    push @params, "T$i arg$i";
+  }
+  return join ", ", @params;
+}
+
+sub suffix {
+  my ($arity) = @_;
+  if ($arity == 1) {
+    return "";
+  } else {
+    return "$arity";
+  }
+}
+
+main();
